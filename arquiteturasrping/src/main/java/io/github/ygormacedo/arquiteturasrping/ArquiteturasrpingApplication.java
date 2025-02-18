a2761da259077e91c8b9de308bb8a6a12575771f
ygormacedo <ygormacedo44@gmail.com>
2025-02-18 12:23:01 -0300

    Initial commit

---
+++ arquiteturasrping/src/main/java/io/github/ygormacedo/arquiteturasrping/ArquiteturasrpingApplication.java
@@ -0,0 +1,13 @@
+package io.github.ygormacedo.arquiteturasrping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturasrpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturasrpingApplication.class, args);
+	}
+
+}
---
+++ arquiteturasrping/src/test/java/io/github/ygormacedo/arquiteturasrping/ArquiteturasrpingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ygormacedo.arquiteturasrping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturasrpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
