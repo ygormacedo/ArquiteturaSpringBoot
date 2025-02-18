3efbbe6a0d17be9b2278e598a1616a1fbe9873d0
ygormacedo <ygormacedo44@gmail.com>
2025-02-18 20:39:30 -0300

    interface annotation

---
+++ arquiteturasrping/src/main/java/io/github/ygormacedo/arquiteturasrping/todos/todosEntily.java
@@ -0,0 +1,17 @@
+package io.github.ygormacedo.arquiteturasrping.todos;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_todo")
+public class todosEntily {
+
+    private Integer id;
+
+    private String descricao;
+
+    private Boolean
+
+}
