7f5126fd5462f8a56d8be8248aa9df20bd4fc5a7
ygormacedo <ygormacedo44@gmail.com>
2025-02-18 22:07:09 -0300

    To-DO COMPLETO PASSANDO A MIL NO POSTMAN

--- arquiteturasrping/src/main/java/io/github/ygormacedo/arquiteturasrping/todos/TodoController.java
+++ arquiteturasrping/src/main/java/io/github/ygormacedo/arquiteturasrping/todos/TodoController.java
@@ -1,13 +1,10 @@
 package io.github.ygormacedo.arquiteturasrping.todos;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("todos")
+@RequestMapping("/todos")
 public class TodoController {
 
     private TodoService service;
@@ -19,5 +16,20 @@ public class TodoController {
     @PostMapping
     public TodoEntity salvar(@RequestBody TodoEntity todo){
       return this.service.salvar(todo);
+    }
+
+    @PutMapping("{id}")
+    public void atualizarStatus(
+            @PathVariable("id") Integer id, @RequestBody TodoEntity todo){
+
+        todo.setId(id);
+
+        service.atualizarStatus(todo);
+
+    }
+
+    @GetMapping("{id}")
+    public TodoEntity buscar(@PathVariable("id") Integer id){
+        return service.buscarPorId(id);
     }
 }
--- arquiteturasrping/src/main/java/io/github/ygormacedo/arquiteturasrping/todos/TodoService.java
+++ arquiteturasrping/src/main/java/io/github/ygormacedo/arquiteturasrping/todos/TodoService.java
@@ -1,7 +1,10 @@
 package io.github.ygormacedo.arquiteturasrping.todos;
 
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Service
 public class TodoService {
@@ -13,8 +16,14 @@ public class TodoService {
     }
 
     public TodoEntity salvar(TodoEntity novoTodo){
-
         return repository.save(novoTodo);
+    }
 
+    public void atualizarStatus(TodoEntity todo){
+        repository.save(todo);
+    }
+
+    public TodoEntity buscarPorId(Integer id){
+        return repository.findById(id).orElse(null);
     }
 }
