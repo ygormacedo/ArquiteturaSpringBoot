bfe4ab197c9ae9ba69c51eb244c3a5c2add028ff
ygormacedo <ygormacedo44@gmail.com>
2025-02-19 20:20:53 -0300

    Lazy

--- arquiteturasrping/src/main/java/io/github/ygormacedo/arquiteturasrping/BeanGerenciado.java
+++ arquiteturasrping/src/main/java/io/github/ygormacedo/arquiteturasrping/BeanGerenciado.java
@@ -3,10 +3,21 @@ package io.github.ygormacedo.arquiteturasrping;
 
 import io.github.ygormacedo.arquiteturasrping.todos.TodoEntity;
 import io.github.ygormacedo.arquiteturasrping.todos.TodoValidator;
+import org.apache.catalina.startup.WebAnnotationSet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
+@Lazy
 @Component
+// @Scope("singleton") utilizado para uma aplicacao
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+//@Scope(WebApplicationContext.SCOPE_APPLICATION)
+//@Scope("request")
+//@Scope("session") esses dois commit sao utilizados para requisicoes web igual o primeiro acima comentando
 public class BeanGerenciado {
 
     @Autowired
